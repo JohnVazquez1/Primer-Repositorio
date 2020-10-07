cff80a79d09957d2091f37ac8e84ce8a6ba0e1d6
JohnVazquez1 <72407073+JohnVazquez1@users.noreply.github.com>
2020-10-06 21:54:50 -0500

    Creación de Presentación.c
    Se creó archivo con código fuente.

---
+++ Presentación.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+Creado el: 5 de octubre
+Creado por: John Vázquez
+*/
+int main(){
+	//Comienza
+	printf("Hola Mundo");
+	printf("Me llamo Jonathan Pablo Vázquez Pérez");
+	return 0;
+}
